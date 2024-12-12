ef0b06b4a20d6cdb8406e16d5616bba9d120bacd
david <davidtall@qq.com>
2024-12-12 17:09:12 +0800

    调整

--- diy.sh
+++ diy.sh
@@ -7,15 +7,24 @@ WRT_REPO='https://github.com/VIKINGYFY/immortalwrt'
 WRT_BRANCH='main'
 
 export WRT_DIR=wrt
-export WRT_TARGET=JDC-AX1800-PRO-WIFI-NO
 export GITHUB_WORKSPACE=$(pwd)
 export WRT_DATE=$(TZ=UTC-8 date +"%y.%m.%d_%H.%M.%S")
 export WRT_VER=$(echo $WRT_REPO | cut -d '/' -f 5-)-$WRT_BRANCH
 export WRT_TYPE=$(sed -n "1{s/^#//;s/\r$//;p;q}" ./Config/$WRT_TARGET.txt)
 export WRT_NAME='OWRT'
-export WRT_WIFI='OWRT'
+export WRT_SSID='OWRT'
+export WRT_WORD='12345678'
 export WRT_THEME='argon'
-export WRT_IP='192.168.1.1'
+export WRT_IP='192.168.10.1'
+export WRT_CI='VIKINGYFY-OpenWRT-CI'
+
+if [ -n "$1" ]; then
+    # 如果有传递参数，赋值给WRT_TARGET
+    export WRT_TARGET="$1"
+else
+    # 如果没有传递参数，设置默认值
+    export WRT_TARGET="Config/JDC-AX1800-PRO-WIFI-NO.txt"
+fi
 
 if [ ! -d $WRT_DIR ]; then
   git clone --depth=1 --single-branch --branch $WRT_BRANCH $WRT_REPO $WRT_DIR
@@ -33,7 +42,7 @@ cd package/
 $GITHUB_WORKSPACE/Scripts/Packages.sh
 $GITHUB_WORKSPACE/Scripts/Handles.sh
 cd ..
-cat $GITHUB_WORKSPACE/Config/$WRT_TARGET.txt $GITHUB_WORKSPACE/Config/GENERAL.txt > .config
+cat $GITHUB_WORKSPACE/$WRT_TARGET $GITHUB_WORKSPACE/Config/GENERAL.txt > .config
 $GITHUB_WORKSPACE/Scripts/Settings.sh
 
 make defconfig
