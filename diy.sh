57d548ff14aa89f86560dd357ee84f15b5276f89
david <269700927@qq.com>
2024-12-15 19:35:37 +0800

    调整

--- diy.sh
+++ diy.sh
@@ -4,6 +4,7 @@
 #WRT_BRANCH='openwrt-24.10'
 
 WRT_REPO='https://github.com/VIKINGYFY/immortalwrt'
+#WRT_REPO='https://github.com/davidtall/VIKINGYFY-immort'
 WRT_BRANCH='main'
 
 if [ -n "$1" ]; then
