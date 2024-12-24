a98d89361cf2ee1a296e4119eaa0ae00cfde3849
david <davidtall@qq.com>
2024-12-24 11:17:14 +0800

    更新编译dead 相关

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -92,3 +92,6 @@ rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypa
 rm -rf ../feeds/packages/net/{shadowsocks-rust,shadowsocksr-libev,xray*,v2ray*,dae*,sing-box,geoview}
 #coremark修复
 sed -i 's/mkdir \$(PKG_BUILD_DIR)\/\$(ARCH)/mkdir -p \$(PKG_BUILD_DIR)\/\$(ARCH)/g' ../feeds/packages/utils/coremark/Makefile
+
+#daed编译需要的依赖
+mkdir -p libcron && wget -O libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
