c754b540b68c1a15ea3e1184ade966fd67f3576b
david <davidtall@qq.com>
2024-12-29 23:24:12 +0800

    nss 12.2

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -68,8 +68,9 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
 	#设置NSS版本
   echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
-  echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
+  echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
 fi
+
 add_kernel_config() {
   if [ -f $1 ]; then
     cat >> $1 <<EOF
