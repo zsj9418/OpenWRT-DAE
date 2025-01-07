38b9f9e28b339df35db37a5aa700bb1a2baa76c2
david <269700927@qq.com>
2025-01-07 23:09:35 +0800

    恢复主题

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -89,6 +89,7 @@ rm -rf small/luci-app-bypass
 sed -i 's/-\(\$(PKG_RELEASE)\)/.\1/g' small/v2ray-geodata/Makefile
 rm -rf luci-app-pushbot && git clone https://github.com/zzsj0928/luci-app-pushbot luci-app-pushbot
 rm -rf luci-app-filemanager && git clone https://github.com/sbwml/luci-app-filemanager
+rm -rf luci-theme-design && git clone https://github.com/SAENE/luci-theme-design
 
 #删除官方的默认插件
 rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypass*}
