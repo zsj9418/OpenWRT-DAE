aa1ece8dbfe4ce17af25e8a4d3fcc3374ba1c025
david <davidtall@qq.com>
2025-01-26 15:17:35 +0800

    添加v2ray-geodata-updater

--- package/v2ray-geodata/init.sh
+++ package/v2ray-geodata/init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-chmod +x /bin/v2ray-geodata-updater
 sed -i '/v2ray-geodata-updater/d' /etc/crontabs/root
 echo "0 4 * * * /bin/v2ray-geodata-updater" >> /etc/crontabs/root
 crontab /etc/crontabs/root
