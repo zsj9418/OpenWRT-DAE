e236cf177026507bb70442cbdb02750e967a814b
davidtall <269700927@qq.com>
2025-05-15 14:23:25 +0800

    回滚daed低版本

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -146,4 +146,5 @@ sed -i "/^.main .main-left .nav li a {/,/^}/ { /font-weight: bolder/d }" $argon_
 sed -i '/^\[data-page="admin-system-opkg"\] #maincontent>.container {/,/}/ s/font-weight: 600;/font-weight: normal;/' $argon_css_file
 
 #修复daed/Makefile
-rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
+cd luci-app-daed && git checkout e0a46c5b6ca2cfbde4a1cb9c345b4fb12a4a8cff
+cat daed/Makefile
