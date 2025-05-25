5f673c2673456c4a433938a1c8accbbf64208452
david <davidtall@qq.com>
2025-05-25 16:19:07 +0800

    取消mr7350

--- Scripts/function.sh
+++ Scripts/function.sh
@@ -120,6 +120,7 @@ function set_kernel_size() {
   sed -i "/^define Device\/jdcloud_re-cs-02/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
   sed -i "/^define Device\/jdcloud_re-cs-07/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
   sed -i "/^define Device\/redmi_ax5-jdcloud/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+  sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
 }
 #开启内存回收补丁
 function enable_skb_recycler() {
