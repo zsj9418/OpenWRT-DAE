e68f67c4dc27a7fccb0b186d4d2c82054fab22e0
david <davidtall@qq.com>
2025-05-27 20:45:29 +0800

    开启sqm

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -61,8 +61,8 @@ if [[ $WRT_TARGET == *"QUALCOMMAX"* ]]; then
 	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
 	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	#开启sqm-nss插件
-#	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
-#	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
+	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
 	#无WIFI配置调整Q6大小
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
 		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
